b161b7a2e1ecfc4f5bf8642983e94d5076c74c77
sunnaylya <79587455+sunnaylya@users.noreply.github.com>
2025-02-05 16:51:41 +0500

    Add files via upload

---
+++ JS_1-2_prj1/index.js
@@ -0,0 +1,15 @@
+// JavaScript-код с созданием переменных
+/*
+Автор:
+Сунагатова Найля
+*/
+const myName = "Naylya";
+console.log(myName);
+let mySurname = "Sunagatova";
+console.log(mySurname);
+let myFavoriteDrink = "tea";
+console.log(myFavoriteDrink);
+let myFavoriteAnimal = "cats";
+console.log(myFavoriteAnimal);
+let myFavoriteProgrammingLanguage = "JavaScript";
+console.log(myFavoriteProgrammingLanguage);
